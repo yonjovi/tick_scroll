d21c98fd22cecf8002217e6a2d3d81227805ceae
Yonatan Rotem <yonatanrotem@Yonatans-Mac-Studio.local>
2023-02-22 22:57:40 +1100

    second commit

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+db_username = os.environ['DB_USERNAME']
+db_password = os.environ['DB_PASSWORD']
+db_host = os.environ['DB_HOST']
+db_name = os.environ['DB_NAME']
 
-# Create database engine
-SQLALCHEMY_DATABASE_URL = 'sqlite:///share_scroller.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
